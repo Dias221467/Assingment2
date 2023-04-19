1b408e03ca093a0135843890b66572a3519dd040
Диас <lbrjyz05@mail.ru>
2023-04-19 20:43:07 +0600

    1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args){
+        MyArrayList arrayList = new MyArrayList();
+        arrayList.add(5);
+        arrayList.add(10);
+        arrayList.add(15);
+        arrayList.add(20);
+        arrayList.add(25);
+
+        System.out.println(arrayList.getElement(3));
+        arrayList.deleteElement(3);
+        System.out.println(arrayList.getElement(3));
+
+        arrayList.clear();
+        if(arrayList.isEmpty())
+            System.out.println("empty");
+        else
+            System.out.println("not");
+    }
+}
