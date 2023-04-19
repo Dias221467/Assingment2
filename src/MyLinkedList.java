90e64feeca3fb69eba05f9444b9bc6515e5f94e4
Диас <lbrjyz05@mail.ru>
2023-04-19 21:50:26 +0600

    6

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,13 +1,21 @@
 public class MyLinkedList<T> implements MyList {
-    private static class Node<E> {
-        E item;
-        Node<E> next;
-        Node<E> prev;
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+        Node<T> prev;
 
-        Node(Node<E> prev, E element, Node<E> next) {
+        Node(Node<T> prev, T element, Node<T> next) {
             this.item = element;
             this.next = next;
             this.prev = prev;
         }
+    }
+    private Node head;
+    private Node tail;
+    private int size;
+    public MyLinkedList(){
+        this.head=null;
+        this.tail=null;
+        size=0;
     }
 }
