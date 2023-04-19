a2ee96768ac515ea64f58ee9b13170861bb45615
Диас <lbrjyz05@mail.ru>
2023-04-19 20:58:04 +0600

    2

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,2 +1,22 @@
-public class MyArrayList {
+public class MyArrayList<T> implements MyList{
+    private T[] arr;
+    private int size;
+
+    public MyArrayList(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+    public void add(T value){
+        if(size==arr.length){
+            increaseLenght();
+        }
+        this.arr[size++] = value;
+    }
+    public void increaseLenght(){
+        T[] newArr =(T[]) new Object[arr.length*2];
+        for(int i=0; i<arr.length; i++){
+            newArr[i] = arr[i];
+        }
+        arr = newArr;
+    }
 }
