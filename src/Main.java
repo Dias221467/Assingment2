576897796693276034d8b3443dd7b0dd036fea6c
Диас <lbrjyz05@mail.ru>
2023-04-20 00:25:31 +0600

    20

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,33 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        /*
         MyArrayList arrayList = new MyArrayList();
         arrayList.add(5);
         arrayList.add(10);
         arrayList.add(15);
         arrayList.add(20);
         arrayList.add(25);
-
-        System.out.println(arrayList.get(3));
-        arrayList.remove(3);
-        System.out.println(arrayList.get(3));
+        System.out.println(arrayList.getElement(3));
+        arrayList.deleteElement(3);
+        System.out.println(arrayList.getElement(3));
 
         arrayList.clear();
-        if(arrayList.isEmpty())
+        if(arrayList.isEmpty()){
             System.out.println("empty");
-        else
+            }
+        else{
             System.out.println("not");
+            }
+              */
+        MyLinkedList lists = new MyLinkedList();
+        lists.add(667);
+        lists.add(228);
+        lists.add(1337);
+        lists.add(1488, 2);
+        lists.printList();
+        System.out.println(lists.size());
+        System.out.println(lists.isSortable());
+        lists.sort();
+        lists.printList();
     }
 }
